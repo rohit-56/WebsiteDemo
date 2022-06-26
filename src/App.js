9581bc4a8388af216bb18267d37d5ad8b5b97afa
Rohit <rohit.sharma2@hexaviewtech.com>
2022-06-26 14:23:45 +0530

    add react routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import "./App.css";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
+import Service from "./components/service/Service";
+import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Header />
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/service" element={<Service />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
