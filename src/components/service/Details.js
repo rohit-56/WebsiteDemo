ef25d04195d0e11b4ca12e148594a1dca97e381c
Rohit <rohit.sharma2@hexaviewtech.com>
2022-06-26 14:23:58 +0530

    details of business units

---
+++ src/components/service/Details.js
@@ -0,0 +1,20 @@
+const data = [
+  {
+    Title: "Enterprise Product Development",
+    Description: "this is the description for EPD",
+  },
+  {
+    Title: "Salesforce",
+    Description: "this is the description for salesforce",
+  },
+  {
+    Title: "Cloud Development",
+    Description: "this is the description for cloud",
+  },
+  {
+    Title: "Data Science",
+    Description: "this is the description for data science",
+  },
+];
+
+export default data;
